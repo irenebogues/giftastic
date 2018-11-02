685912ffd82a8364468993d277c102f84bf1e1dc
Irene Bogues <irenebogues@gmail.com>
2018-11-02 14:51:35 -0400

    writing pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
 // Giphy API Key PXyBdtBKhwr5zsbH773K4W58yrUOQG3r
 
+ // Start with a short Array for Good Doggies
+    // (function populate()) populate the array items to the topic-btns
+    // if the topic-btn is clicked
+        // (function)
+        // make an AJAX call
+        // Get the GIF for the corresponding topic-btn
+        // display 10 static imgs in the page
+        // if img is clicked and !animate 
+            // change static image for animated gif
+        // if img is clicked and animate
+            // change animated gif to static img
+    // if btn from the form is clicked
+        // get input value
+        // push it to the topic-array
+        // (function populate()) populate items to the btns
 
